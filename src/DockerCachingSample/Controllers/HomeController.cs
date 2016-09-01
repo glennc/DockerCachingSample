5ea5f1958c689e1aea4f00113c65b46eaf483390
glennc <glennc@microsoft.com>
2016-09-01 14:09:07 -0700

    initial

---
+++ src/DockerCachingSample/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.Extensions.Logging;
+
+namespace DockerCachingSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        //Cache in any proxiesm including proxies.
+        [ResponseCache(Duration = 30, Location = ResponseCacheLocation.Any)]
+        public IActionResult Index()
+        {
+            _logger.LogCritical("Reached View Controller");
+            return View();
+        }
+
+        //My about page never changes so it will cache for a day
+        [ResponseCache(Duration = 86400, Location = ResponseCacheLocation.Any)]
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        //My contact page never changes so it will cache for a day
+        [ResponseCache(Duration = 86400, Location = ResponseCacheLocation.Any)]
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        //Explicitly don't cache Error page. This shouldn't strictly be required since
+        //we shouldn't cache at all by default for non 200 and none is the default location.
+        [ResponseCache(Location = ResponseCacheLocation.None)]
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/DockerCachingSample/Startup.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Text;
+
+namespace DockerCachingSample
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDistributedRedisCache(options =>
+            {
+                options.Configuration = "127.0.0.1";
+                options.InstanceName = "SampleInstance";
+            });
+            services.AddDistributedResponseCache();
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            var logger = loggerFactory.CreateLogger<Startup>();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.Use(async (context, next) =>
+            {
+                var headers = context.Request.Headers.ToList();
+                var reqBuilder = new StringBuilder();
+                headers.ForEach(x =>reqBuilder.AppendLine($"{x.Key}={x.Value}"));
+                logger.LogCritical(reqBuilder.ToString());
+                logger.LogCritical("Middleware running.");
+                await next();
+                reqBuilder.Clear();
+                context.Response.Headers.ToList().ForEach(x => reqBuilder.AppendLine($"{x.Key}={x.Value}"));
+                logger.LogCritical(reqBuilder.ToString());
+            });
+
+            app.UseResponseCaching();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
